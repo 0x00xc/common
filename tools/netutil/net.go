e42267e6f60aeebc364c2c7a1940054e09bfdeca
laixingchen <laixingchen@beifangjing.com>
2022-03-03 15:49:34 +0800

    netutil

---
+++ tools/netutil/net.go
@@ -0,0 +1,59 @@
+package netutil
+
+import "net"
+
+// GetLocalIPv4s return all non-loopback IPv4 addresses
+func GetLocalIPv4s() ([]string, error) {
+	var ips []string
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ips, err
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ips = append(ips, ipnet.IP.String())
+		}
+	}
+
+	return ips, nil
+}
+
+// GetIPv4ByInterface return IPv4 address from a specific interface IPv4 addresses
+func GetIPv4ByInterface(name string) ([]string, error) {
+	var ips []string
+
+	iface, err := net.InterfaceByName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	addrs, err := iface.Addrs()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			ips = append(ips, ipnet.IP.String())
+		}
+	}
+
+	return ips, nil
+}
+
+// GetLocalIp 获取本机IP地址
+// @return string
+func GetLocalIp() (string, error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP.String(), nil
+}
