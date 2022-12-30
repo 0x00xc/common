6c0d20a32fe115cee51c9b9dcdbec7e5f6ca6c30
Xingchen <laixingchen@aosign.cn>
2022-12-30 13:28:00 +0800

    update args

--- args/args.go
+++ args/args.go
@@ -5,7 +5,13 @@ import (
 	"strings"
 )
 
+// ParseArgs
+// Deprecated: use Parse() instead
 func ParseArgs() (map[string]string, []string) {
+	return parse()
+}
+
+func parse() (map[string]string, []string) {
 	var args = os.Args[1:]
 	var i = 0
 	var k string
@@ -32,4 +38,43 @@ func ParseArgs() (map[string]string, []string) {
 		kv[k] = ""
 	}
 	return kv, single
+}
+
+type KV map[string]string
+type Cmd []string
+
+func (kv KV) Is(options ...string) bool {
+	for _, opt := range options {
+		if _, ok := kv[opt]; ok {
+			return ok
+		}
+	}
+	return false
+}
+
+func (kv KV) Val(options ...string) string {
+	for _, opt := range options {
+		if val, ok := kv[opt]; ok {
+			return val
+		}
+	}
+	return ""
+}
+
+func (c Cmd) Cmd() string {
+	if len(c) > 0 {
+		return c[0]
+	}
+	return ""
+}
+
+func (c Cmd) Options() []string {
+	if len(c) > 1 {
+		return c[1:]
+	}
+	return []string{}
+}
+
+func Parse() (KV, Cmd) {
+	return parse()
 }
