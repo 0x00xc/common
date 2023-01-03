c930d0a5c67462227310001e79f3b5f8626932fd
Xingchen <laixingchen@aosign.cn>
2023-01-03 09:52:02 +0800

    update

--- args/args.go
+++ args/args.go
@@ -5,28 +5,26 @@ import (
 	"strings"
 )
 
-// ParseArgs
-// Deprecated: use Parse() instead
-func ParseArgs() (map[string]string, []string) {
-	return parse()
-}
-
-func parse() (map[string]string, []string) {
+func parse() (map[string][]string, []string) {
 	var args = os.Args[1:]
 	var i = 0
 	var k string
 	var single []string
-	var kv = map[string]string{}
+	var kv = map[string][]string{}
 	for i < len(args) {
 		val := args[i]
 		if strings.HasPrefix(val, "-") {
 			if k != "" {
-				kv[k] = ""
+				if _, ok := kv[k]; !ok {
+					kv[k] = []string{}
+				} else {
+
+				}
 			}
 			k = val[1:]
 		} else {
 			if k != "" {
-				kv[k] = val
+				kv[k] = append(kv[k], val)
 				k = ""
 			} else {
 				single = append(single, val)
@@ -35,12 +33,14 @@ func parse() (map[string]string, []string) {
 		i++
 	}
 	if k != "" {
-		kv[k] = ""
+		if _, ok := kv[k]; !ok {
+			kv[k] = []string{}
+		}
 	}
 	return kv, single
 }
 
-type KV map[string]string
+type KV map[string][]string
 type Cmd []string
 
 func (kv KV) Is(options ...string) bool {
@@ -55,10 +55,23 @@ func (kv KV) Is(options ...string) bool {
 func (kv KV) Val(options ...string) string {
 	for _, opt := range options {
 		if val, ok := kv[opt]; ok {
-			return val
+			if len(val) > 0 {
+				return val[0]
+			} else {
+				return ""
+			}
 		}
 	}
 	return ""
+}
+
+func (kv KV) Vals(options ...string) []string {
+	for _, opt := range options {
+		if val, ok := kv[opt]; ok {
+			return val
+		}
+	}
+	return nil
 }
 
 func (c Cmd) Cmd() string {
